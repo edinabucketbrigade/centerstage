e456b756e67837757953a640d745ec33d8454349
clarkmilbert <clarkmilbert@gmail.com>
2023-09-07 19:05:55 -0500

    Added TeleOpB

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpB.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TeleOpB extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+
+    }
+
+}
