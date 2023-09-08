111f231b66dd61c1f7a990b2d08f2b7915b291e7
Jack Beatty <jackbeatty2005@gmail.com>
2023-09-07 19:23:04 -0500

    Update TeleOpA.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpA.java
@@ -6,8 +6,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class TeleOpA extends LinearOpMode {
 
+    Servo servo;
+    double position = (MAX_POS - MIN_POS) / 2;
+    boolean rampUp = true;
+
     @Override
     public void runOpMode() {
+        //turn servo 90
+        servo = new hardwareMap.get(Servo);
+        servo.turn(90);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
