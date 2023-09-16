bf75dc776495271dd3c065b07d9ccb1e5d47225a
Jack Beatty <jackbeatty2005@gmail.com>
2023-09-16 10:37:45 -0500

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,7 +14,7 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                                .forward(40)
                                 .turn(Math.toRadians(90))
                                 .forward(30)
                                 .turn(Math.toRadians(90))
