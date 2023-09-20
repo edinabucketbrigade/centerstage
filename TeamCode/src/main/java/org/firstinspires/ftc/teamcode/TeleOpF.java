31db2f932c6bd7069bb090f0e35b1ac0816cb663
2strap <142550915+2strap@users.noreply.github.com>
2023-09-19 19:27:03 -0500

    bora created an opmode through bore encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpF.java
@@ -2,19 +2,25 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class TeleOpA extends LinearOpMode {
+public class TeleOpF extends LinearOpMode {
+    private DcMotor motor = null;
 
     @Override
     public void runOpMode() {
-
+        motor  = hardwareMap.get(DcMotor.class, "encoder");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
         while (opModeIsActive()) {
+            int position = motor.getCurrentPosition();
+            telemetry.addData("Encoder Position", position);
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
