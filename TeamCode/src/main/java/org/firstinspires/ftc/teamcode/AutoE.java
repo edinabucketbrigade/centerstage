55b1d9c124ed35bd6f16ff7753c4b78505499797
nsarge09 <norasargeant09@gmail.com>
2023-10-03 18:55:35 -0500

    Added AutoE

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoE.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(preselectTeleOp = "TeleOpE")
+public class AutoE extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+
+    }
+
+}
