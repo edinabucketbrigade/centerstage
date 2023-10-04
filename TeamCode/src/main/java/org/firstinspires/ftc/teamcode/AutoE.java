336eac007c9d9600c847a6becf89e735128baede
nsarge09 <norasargeant09@gmail.com>
2023-10-03 19:37:22 -0500

    Update AutoE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoE.java
@@ -6,24 +6,47 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(preselectTeleOp = "TeleOpE")
 public class AutoE extends LinearOpMode {
 
-    boolean red;
+    Boolean red = null;
+    Boolean left = null;
+    Boolean direct = null;
     boolean initialized = false;
     @Override
     public void runOpMode() {
 
-        telemetry.addData("Status", "X = blue, B = red");
-        telemetry.update();
 
         while (!initialized) {
-            if (gamepad1.x) {
-                red = false;
-                initialized = true;
+            if (red == null) {
+                telemetry.addData("Status", "X = blue, B = red");
+                telemetry.update();
+                if (gamepad1.x) {
+                    red = false;
+                } else if (gamepad1.b) {
+                    red = true;
+                }
             }
-            else if (gamepad1.b) {
-                red = true;
+            else if (left == null){
+                telemetry.addData("Status", "X = left, B = right");
+                telemetry.update();
+                if (gamepad1.x) {
+                    left = true;
+                } else if (gamepad1.b) {
+                    left = false;
+                }
+            }
+            else if (direct == null) {
+                telemetry.addData("Status", "X = direct, B = indirect");
+                telemetry.update();
+                if (gamepad1.x) {
+                    direct = true;
+                } else if (gamepad1.b) {
+                    direct = false;
+                }
+            }
+            else {
                 initialized = true;
             }
         }
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
