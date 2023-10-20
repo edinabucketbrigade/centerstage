ec4a998e3b5a314f7fe1b18ca3ee80f6e831e118
Randy Milbert <randy.milbert@primitive.us>
2023-10-19 20:39:40 -0500

    Avoided setting ready value multiple times

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpK.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpK.java
@@ -44,7 +44,7 @@ public class TeleOpK extends LinearOpMode {
 
             boolean isPressed = touchSensor.isPressed();
 
-            if (isPressed) {
+            if (isPressed && !isReady) {
                 armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 isReady = true;
