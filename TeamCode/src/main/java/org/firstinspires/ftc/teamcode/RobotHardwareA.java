b474d69c643352bdc46982e49881eddf810b6e85
Randy Milbert <randy.milbert@primitive.us>
2023-11-02 09:31:17 -0500

    Changed wrist down position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardwareA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardwareA.java
@@ -43,7 +43,7 @@ public class RobotHardwareA {
     public static double LEFT_CLAW_CLOSED_POSITION = 1;
     public static double RIGHT_CLAW_CLOSED_POSITION = 0.275;
     public static double RIGHT_CLAW_OPEN_POSITION = 0.6;
-    public static double WRIST_DOWN_POSITION = 0;
+    public static double WRIST_DOWN_POSITION = 0.03;
     public static double WRIST_UP_POSITION = 0.72;
     public static int ARM_DOWN_POSITION = 0;
     public static int ARM_UP_POSITION = 1300;
