53a91774c9ba935f3213e3d5ca11748d3d929bb8
Jack Beatty <jackbeatty2005@gmail.com>
2023-12-02 11:28:31 -0600

    Update PixelDetection.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PixelDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PixelDetection.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import android.app.Activity;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -16,11 +17,24 @@ import android.view.View;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+@Config
 @Autonomous
 public class PixelDetection extends LinearOpMode {
     NormalizedColorSensor colorSensor;
-    View relativeLayout;
-    public static float gain = 1f;
+    RevBlinkinLedDriver blinkinLedDriver;
+    RevBlinkinLedDriver.BlinkinPattern blinkinPattern;
+
+    public static float gain = 0.8f;
+    public static double whiteValue = 0.5;
+    public static int yellowMin = 20;
+    public static int yellowMax = 80;
+    public static int greenMin = 100;
+    public static int greenMax = 130;
+    public static int purpleMin = 180;
+    public static int purpleMax = 290;
+
+
+
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -30,6 +44,10 @@ public class PixelDetection extends LinearOpMode {
             ((SwitchableLight)colorSensor).enableLight(true);
         }
 
+        blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
+        blinkinPattern = RevBlinkinLedDriver.BlinkinPattern.RED;
+        blinkinLedDriver.setPattern(blinkinPattern);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -38,7 +56,7 @@ public class PixelDetection extends LinearOpMode {
         while (opModeIsActive()) {
             colorSensor.setGain(gain);
             NormalizedRGBA colors = colorSensor.getNormalizedColors();
-            Color.colorToHSV(colors.toColor(),hsvValues);
+            Color.colorToHSV(colors.toColor(), hsvValues);
             telemetry.addLine()
                     .addData("Red", "%.3f", colors.red)
                     .addData("Green", "%.3f", colors.green)
@@ -48,20 +66,30 @@ public class PixelDetection extends LinearOpMode {
                     .addData("Saturation", "%.3f", hsvValues[1])
                     .addData("Value", "%.3f", hsvValues[2]);
             telemetry.addData("Alpha", "%.3f", colors.alpha);
-            if (hsvValues[1] <= 0.3)
-                telemetry.addData("Color Detected Is: ", "White");
-            else if (hsvValues[0] >= 20 && hsvValues[0] <= 70)
-                telemetry.addData("Color Detected Is: ", "Yellow");
-            else if (hsvValues[0] >= 90 && hsvValues[0] <= 130)
-                telemetry.addData("Color Detected Is: ", "Green");
-            else if (hsvValues[0] >= 240 && hsvValues[0] <= 290)
-                telemetry.addData("Color Detected Is: ", "Purple");
+
+            if (hsvValues[1] <= whiteValue)
+                //telemetry.addData("Color Detected Is: ", "White");
+                blinkinPattern = RevBlinkinLedDriver.BlinkinPattern.WHITE;
+            else if (hsvValues[0] >= yellowMin && hsvValues[0] <= yellowMax)
+                //telemetry.addData("Color Detected Is: ", "Yellow");
+                blinkinPattern = RevBlinkinLedDriver.BlinkinPattern.YELLOW;
+            else if (hsvValues[0] >= greenMin && hsvValues[0] <= greenMax)
+                //telemetry.addData("Color Detected Is: ", "Green");
+                blinkinPattern = RevBlinkinLedDriver.BlinkinPattern.GREEN;
+            else if (hsvValues[0] >= purpleMin && hsvValues[0] <= purpleMax)
+                //telemetry.addData("Color Detected Is: ", "Purple");
+                blinkinPattern = RevBlinkinLedDriver.BlinkinPattern.VIOLET;
             else
-                telemetry.addLine("No color in valid range.");
+                //telemetry.addLine("No color in valid range.");
+                blinkinPattern = RevBlinkinLedDriver.BlinkinPattern.RED;
+
 
             if (colorSensor instanceof DistanceSensor) {
                 telemetry.addData("Distance (cm)", "%.3f", ((DistanceSensor) colorSensor).getDistance(DistanceUnit.CM));
             }
+
+            blinkinLedDriver.setPattern(blinkinPattern);
+            telemetry.addData("Color Is", blinkinPattern);
 
             telemetry.addData("Status", "Running");
             telemetry.update();
