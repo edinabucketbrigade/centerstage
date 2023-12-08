378ae6f843c0e05856b924a8626096036d81ea00
clarkmilbert <clarkmilbert@gmail.com>
2023-12-07 18:02:33 -0600

    changed meep meep version and enabled tele op b

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -30,7 +30,7 @@ enum Route {
 
 public class MeepMeepTesting {
 
-    public static final Route ROUTE = Route.PARK_LEFT;
+    public static final Route ROUTE = Route.RED_LEFT_DIRECT;
     public static final double DELAY = 0.5;
     public static final double MAXVEL = 60;
     public static final double MAXACCEL = 60;
