0e7c796ca850c86dfd8e1739b5491922ef5b6f60
Jack Beatty <jackbeatty2005@gmail.com>
2023-12-14 19:57:05 -0600

    updatwe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraPixelAutoDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraPixelAutoDetection.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -19,11 +22,26 @@ public class CameraPixelAutoDetection extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        Gamepad previousGamepad = new Gamepad();
+        Gamepad currentGamepad = new Gamepad();
+
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         CameraPixelColor detector = new CameraPixelColor(telemetry);
         camera.setPipeline(detector);
         camera.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
+        if (currentGamepad.x && !previousGamepad.x){
+            Core.inRange(detector.mat,detector.MINIMUM_WHITE,detector.MAXIMUM_WHITE,detector.mat);
+        }
+        if (currentGamepad.a && !previousGamepad.a){
+            Core.inRange(detector.mat,detector.MINIMUM_WHITE,detector.MAXIMUM_WHITE,detector.mat);
+        }
+        if (currentGamepad.b && !previousGamepad.b){
+            Core.inRange(detector.mat,detector.MINIMUM_WHITE,detector.MAXIMUM_WHITE,detector.mat);
+        }
+        if (currentGamepad.y && !previousGamepad.y){
+            Core.inRange(detector.mat,detector.MINIMUM_WHITE,detector.MAXIMUM_WHITE,detector.mat);
+        }
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -56,14 +74,14 @@ public class CameraPixelAutoDetection extends LinearOpMode {
         location = detector.getLocation();
         log("5");
 
-        camera.stopStreaming();
-
-        camera.closeCameraDevice();
-
-        while(opModeIsActive()){
+        while(opModeIsActive()) {
             detector.telemetry.update();
             telemetry.update();
         }
+
+        camera.stopStreaming();
+
+        camera.closeCameraDevice();
     }
     private void log(String message) {
         telemetry.addData("Message", message);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraPixelColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraPixelColor.java
@@ -12,21 +12,21 @@ import org.openftc.easyopencv.OpenCvPipeline;
 @Config
 public class CameraPixelColor extends OpenCvPipeline {
     public static int MINIMUM_WHITE_VALUES = 0;
-    public static int MINIMUM_YELLOW_VALUES = 0;
-    public static int MINIMUM_GREEN_VALUES = 0;
-    public static int MINIMUM_PURPLE_VALUES = 0;
-    public static int MAXIMUM_WHITE_VALUES = 160;
-    public static int MAXIMUM_YELLOW_VALUES = 0;
-    public static int MAXIMUM_GREEN_VALUES = 0;
-    public static int MAXIMUM_PURPLE_VALUES = 0;
+    public static int MINIMUM_YELLOW_VALUES = 40;
+    public static int MINIMUM_GREEN_VALUES = 80;
+    public static int MINIMUM_PURPLE_VALUES = 40;
+    public static int MAXIMUM_WHITE_VALUES = 20;
+    public static int MAXIMUM_YELLOW_VALUES = 255;
+    public static int MAXIMUM_GREEN_VALUES = 255;
+    public static int MAXIMUM_PURPLE_VALUES = 255;
     public static int MINIMUM_WHITE_HUE = 0;
-    public static int MINIMUM_YELLOW_HUE = 0;
-    public static int MINIMUM_GREEN_HUE = 0;
-    public static int MINIMUM_PURPLE_HUE = 0;
-    public static int MAXIMUM_WHITE_HUE = 20;
-    public static int MAXIMUM_YELLOW_HUE = 0;
-    public static int MAXIMUM_GREEN_HUE = 0;
-    public static int MAXIMUM_PURPLE_HUE = 0;
+    public static int MINIMUM_YELLOW_HUE = 40;
+    public static int MINIMUM_GREEN_HUE = 80;
+    public static int MINIMUM_PURPLE_HUE = 115;
+    public static int MAXIMUM_WHITE_HUE = 255;
+    public static int MAXIMUM_YELLOW_HUE = 70;
+    public static int MAXIMUM_GREEN_HUE = 100;
+    public static int MAXIMUM_PURPLE_HUE = 140;
 
     public static Rect LEFT_ROI = new Rect(30,160, 150,120);
     public static Rect RIGHT_ROI = new Rect(435,165, 150,120);
@@ -38,6 +38,9 @@ public class CameraPixelColor extends OpenCvPipeline {
 
     Telemetry telemetry;
     Mat mat = new Mat();
+
+    public Scalar MINIMUM_WHITE;
+    public Scalar MAXIMUM_WHITE;
     public CameraPixelColor(Telemetry telemetry){
         this.telemetry = telemetry;
     }
@@ -45,16 +48,15 @@ public class CameraPixelColor extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
 
-        Scalar MINIMUM_WHITE = new Scalar(MINIMUM_WHITE_HUE,MINIMUM_WHITE_VALUES,MINIMUM_WHITE_VALUES);
-        Scalar MAXIMUM_WHITE = new Scalar(MAXIMUM_WHITE_HUE,MAXIMUM_WHITE_VALUES,MAXIMUM_WHITE_VALUES);
-        Scalar MINIMUM_YELLOW = new Scalar(MINIMUM_YELLOW_HUE,MINIMUM_YELLOW_VALUES,MINIMUM_YELLOW_VALUES);
-        Scalar MAXIMUM_YELLOW = new Scalar(MAXIMUM_YELLOW_HUE,MAXIMUM_YELLOW_VALUES,MAXIMUM_YELLOW_VALUES);
-        Scalar MINIMUM_GREEN = new Scalar(MINIMUM_GREEN_HUE,MINIMUM_GREEN_VALUES,MINIMUM_GREEN_VALUES);
-        Scalar MAXIMUM_GREEN = new Scalar(MAXIMUM_GREEN_HUE,MAXIMUM_GREEN_VALUES,MAXIMUM_GREEN_VALUES);
-        Scalar MINIMUM_PURPLE = new Scalar(MINIMUM_PURPLE_HUE,MINIMUM_PURPLE_VALUES,MINIMUM_PURPLE_VALUES);
-        Scalar MAXIMUM_PURPLE = new Scalar(MAXIMUM_PURPLE_HUE,MAXIMUM_PURPLE_VALUES,MAXIMUM_PURPLE_VALUES);
+        MINIMUM_WHITE = new Scalar(MINIMUM_WHITE_HUE, MINIMUM_WHITE_VALUES, MINIMUM_WHITE_VALUES);
+        MAXIMUM_WHITE = new Scalar(MAXIMUM_WHITE_HUE, MAXIMUM_WHITE_VALUES, MAXIMUM_WHITE_VALUES);
+        Scalar MINIMUM_YELLOW = new Scalar(MINIMUM_YELLOW_HUE, MINIMUM_YELLOW_VALUES, MINIMUM_YELLOW_VALUES);
+        Scalar MAXIMUM_YELLOW = new Scalar(MAXIMUM_YELLOW_HUE, MAXIMUM_YELLOW_VALUES, MAXIMUM_YELLOW_VALUES);
+        Scalar MINIMUM_GREEN = new Scalar(MINIMUM_GREEN_HUE, MINIMUM_GREEN_VALUES, MINIMUM_GREEN_VALUES);
+        Scalar MAXIMUM_GREEN = new Scalar(MAXIMUM_GREEN_HUE, MAXIMUM_GREEN_VALUES, MAXIMUM_GREEN_VALUES);
+        Scalar MINIMUM_PURPLE = new Scalar(MINIMUM_PURPLE_HUE, MINIMUM_PURPLE_VALUES, MINIMUM_PURPLE_VALUES);
+        Scalar MAXIMUM_PURPLE = new Scalar(MAXIMUM_PURPLE_HUE, MAXIMUM_PURPLE_VALUES, MAXIMUM_PURPLE_VALUES);
 
-        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB);
         Core.inRange(mat, MINIMUM_WHITE, MAXIMUM_WHITE, mat);
         Core.inRange(mat, MINIMUM_YELLOW, MAXIMUM_YELLOW, mat);
         Core.inRange(mat, MINIMUM_GREEN, MAXIMUM_GREEN, mat);
@@ -66,18 +68,17 @@ public class CameraPixelColor extends OpenCvPipeline {
         double leftValue = Core.sumElems(left).val[0];
         double rightValue = Core.sumElems(right).val[0];
 
-        telemetry.addData("Left Raw Value", leftValue);
-        telemetry.addData("Right Raw Value", rightValue);
-
         left.release();
         right.release();
 
-        if (rightValue >= leftValue){
+        if (rightValue >= leftValue) {
             location = Location.Right;
         } else {
             location = Location.Left;
         }
 
+        telemetry.addData("Left Raw Value", leftValue);
+        telemetry.addData("Right Raw Value", rightValue);
         return mat;
     }
     public CameraPixelColor.Location getLocation(){return location;}
