4ac3380ac3ee0c0fb840101e827b6362858f1d11
nsarge09 <norasargeant09@gmail.com>
2023-12-21 18:09:51 -0600

    Create TeleOpQ

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpQ.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+@Disabled
+public class TeleOpQ extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+
+    }
+
+}
