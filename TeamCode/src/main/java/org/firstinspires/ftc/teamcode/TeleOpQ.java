5c3e3c45ee667cb8b1d068fb24bf924519c71b9b
XinfiniteX <94420212+XinfiniteX@users.noreply.github.com>
2023-12-21 19:45:11 -0600

    Update TeleOpQ.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpQ.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpQ.java
@@ -14,12 +14,12 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 @TeleOp
 public class TeleOpQ extends LinearOpMode {
 
-    public static double ELBOW_PICK_UP = 0.2;
+    public static double ELBOW_PICK_UP = 0.05;
     public static double ELBOW_BACKDROP = 0.67;
     public static double WRIST_PICK_UP = 0.75;
-    public static double WRIST_BACKDROP = 0.57;
+    public static double WRIST_BACKDROP = 0.2;
     public static double MINIMUM_HAND_POSITION = 0.5;
-    public static double MAXIMUM_HAND_POSITION = 0.6;
+    public static double MAXIMUM_HAND_POSITION = 0;
 
     private Servo elbowServo;
     private Servo wristServo;
@@ -40,6 +40,21 @@ public class TeleOpQ extends LinearOpMode {
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
             telemetry.update();
+            if(gamepad1.a){
+                elbowServo.setPosition(ELBOW_PICK_UP);
+                wristServo.setPosition(WRIST_PICK_UP);
+            }
+            if(gamepad1.y){
+                elbowServo.setPosition(ELBOW_BACKDROP);
+                wristServo.setPosition(WRIST_BACKDROP);
+
+            }
+            if(gamepad1.x){
+                handServo.setPosition(MINIMUM_HAND_POSITION);
+            }
+            if(gamepad1.b){
+                handServo.setPosition(MAXIMUM_HAND_POSITION);
+            }
         }
 
     }
