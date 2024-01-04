42ed986f6761586a8dc03273424e60153f6d49f6
clarkmilbert <clarkmilbert@gmail.com>
2024-01-04 16:22:46 -0600

    added a trajectory sequence to tele op f that drives to the spike mark and then the backdrop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoF.java
@@ -3,10 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
@@ -31,38 +33,71 @@ enum Route {
 public class AutoF extends LinearOpMode {
     public static final Route ROUTE = Route.RED_LEFT_DIRECT;
     public static final double DELAY = 0.5;
-    public static final double MAXVEL = 60;
-    public static final double MAXACCEL = 60;
-    public static final double MAXANGVEL = Math.toRadians(180);
-    public static final double MAXANGACCEL = Math.toRadians(180);
-    public static final double TRACKWIDTH = 15;
-
-    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-    public static TrajectorySequence trajectorySequence(SampleMecanumDrive drive) {
-        
-    }
-
     @Override
     public void runOpMode() {
-
+/*
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                .strafeRight(10)
-                .forward(5)
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d(61, -36, Math.toRadians(180)))
+                .lineTo(new Vector2d(35, -36))
+                //.lineToLinearHeading(new Pose2d(35, 36, Math.toRadians(180)))
+                .splineTo(new Vector2d(35, 36), Math.toRadians(90))
+                .build();
+
+        TrajectorySequence trajectorySequence = drive.trajectorySequenceBuilder(new Pose2d(61, -36, Math.toRadians(180)))
+                .back(26)
+                .turn(Math.toRadians(90))
+                .forward(72)
                 .build();
 
         waitForStart();
 
         if(isStopRequested()) return;
 
-        drive.followTrajectory(myTrajectory);
+        drive.followTrajectorySequence(trajectorySequence);
 
-        /*while (opModeIsActive()) {
+        while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
             telemetry.update();
-        }*/
+        }
+
+        Pose2d poseEstimate = drive.getPoseEstimate();
+        telemetry.addData("finalX", poseEstimate.getX());
+        telemetry.addData("finalY", poseEstimate.getY());
+        telemetry.addData("finalHeading", poseEstimate.getHeading());
+        telemetry.update();
+
+        while (!isStopRequested() && opModeIsActive()) ;
+        */
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(-24, -24, 0);
+
+        drive.setPoseEstimate(startPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        //while (!isStopRequested()) {
+            TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+                    .back(26)
+                    .turn(Math.toRadians(-90))
+                    .back(72)
+                    /*.forward(DISTANCE)
+                    .turn(Math.toRadians(90))
+                    .forward(DISTANCE)
+                    .turn(Math.toRadians(90))
+                    .forward(DISTANCE)
+                    .turn(Math.toRadians(90))*/
+                    .build();
+            drive.followTrajectorySequence(trajSeq);
+        //}
+
     }
 
 }
