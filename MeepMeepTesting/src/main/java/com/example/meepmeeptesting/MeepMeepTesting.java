35079f169301772d3c4b530e8764bfea6fafb966
clarkmilbert <clarkmilbert@gmail.com>
2024-02-03 10:30:53 -0600

    added red left yellow pixel paths to meep meep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -112,7 +112,7 @@ public class MeepMeepTesting {
 
                 // Red left start X
                 // -36
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(12, 61, Math.toRadians(90)))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-36, -61, Math.toRadians(-90)))
                     // Red right
                             // Left
                                 //.lineToLinearHeading(new Pose2d(14,-30, Math.toRadians(0)))
@@ -153,14 +153,25 @@ public class MeepMeepTesting {
 //                                .splineTo(new Vector2d(28,-8), Math.toRadians(0))
 //                                .splineTo(new Vector2d(44, -36), Math.toRadians(0))
 
+                                    .setReversed(true)
+
+//                                .lineToLinearHeading(new Pose2d(-34,-30,Math.toRadians(180)))
+//                                .lineToLinearHeading(new Pose2d(-35,-10,Math.toRadians(180)))
+
+
+//                                .lineToLinearHeading(new Pose2d(-36,-10, Math.toRadians(-90)))
+//                                .turn(Math.toRadians(-90))
+
+
+//                                .splineToLinearHeading(new Pose2d(-37,-30),Math.toRadians(0))
+//                                .setReversed(true)
+//                                .splineTo(new Vector2d(-30,-10), Math.toRadians(0))
+
 
                                 .setReversed(true)
-
-                                //.lineToLinearHeading(new Pose2d(-34,-30,Math.toRadians(180)))
-
-                                //.lineToLinearHeading(new Pose2d(-36,-10, Math.toRadians(-90)))
-
-                                //.splineToLinearHeading(new Pose2d(-37,-30),Math.toRadians(0))
+                                .splineTo(new Vector2d(0,-8),Math.toRadians(0))
+                                .splineTo(new Vector2d(28,-8), Math.toRadians(0))
+                                .splineTo(new Vector2d(44,-36), Math.toRadians(0))
 
 
                                 //.splineToLinearHeading(new Pose2d(13,-30), Math.toRadians(180))
@@ -180,9 +191,11 @@ public class MeepMeepTesting {
 
                                 //.lineToLinearHeading(new Pose2d(13,30,Math.toRadians(180)))
 
+
                                 //.lineToLinearHeading(new Pose2d(12,10, Math.toRadians(90)))
 
                                 //.splineToLinearHeading(new Pose2d(10,30),Math.toRadians(0))
+
 
                                 .build()
                 );
