68fad0a8b814c0deb530de60da8ae422402e5952
Randy Milbert <randy.milbert@primitive.us>
2024-02-07 13:59:29 -0600

    Adjusted claw positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -14,14 +14,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Config
 public class Claw {
 
-    public static double LEFT_CLOSED = 0.3;
+    public static double LEFT_CLOSED = 0.4;
     public static double LEFT_PARTIALLY_OPEN = 0.35;
-    public static double LEFT_FULLY_OPEN = 0.62;
+    public static double LEFT_FULLY_OPEN = 0.67;
     public static double PIXEL_CAPTURE_THRESHOLD = 30;
-    public static double RIGHT_CLOSED = 0.5;
+    public static double RIGHT_CLOSED = 0.55;
     public static double RIGHT_PARTIALLY_OPEN = 0.45;
-    public static double RIGHT_FULLY_OPEN = 0.24;
-    public static double WRIST_DOWN_POSITION = 0.275;
+    public static double RIGHT_FULLY_OPEN = 0.3;
+    public static double WRIST_DOWN_POSITION = 0.265;
     public static double WRIST_UP_POSITION = 0.92;
     public static double WRIST_RELEASE_POSITION = 0.98;
 
