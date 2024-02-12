fb1e719e118bb115b659d942775428d7d900de54
Randy Milbert <randy.milbert@primitive.us>
2024-02-12 10:42:49 -0600

    Sped up arm raise

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -16,9 +16,9 @@ public class Arm {
     public static int DOWN_POSITION = 0;
     public static double LOWER_GAIN = 0.0008;
     public static double MAXIMUM_LOWER_POWER = 0.5;
-    public static double MAXIMUM_RAISE_POWER = 0.3;
+    public static double MAXIMUM_RAISE_POWER = 0.8;
     public static double RAISE_EXPONENT = 2;
-    public static double RAISE_GAIN = 0.000001;
+    public static double RAISE_GAIN = 0.0000018;
     public static int THRESHOLD = 50;
     public static int UP_POSITION = 750;
 
